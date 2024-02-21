33c9db6037bcae2f0331e0f10c62dcef92b3a4b3
AdamWallgren28 <mattef91@hotmail.com>
2024-02-21 11:10:19 +0100

    ads savebutton

--- script.js
+++ script.js
@@ -35,3 +35,28 @@ input.addEventListener('keydown', (event) => {
         addItem();
     }
 });
+
+/*Save/Download button */
+saveBtn.addEventListener('click', () => {
+    alert ("Do you want to download this list?");
+
+    const listContent = Array.from(list.children).map(item => {
+        /*Exkluderar delete-knappen i det sparade txt-dokumentet*/
+        const textElement = item.querySelector('span');
+        if (item.tagName.toLowerCase() === 'li' && textElement) {
+            return textElement.textContent;
+        } else {
+            return '';
+        }
+    }).join('\n');
+    const blob = new Blob([listContent], { type: 'text/plain' });   /*Skapar en blob att lagra texten binärt */
+    const url = URL.createObjectURL(blob);                          /*skapar en url för bloben som browsern kan hantera */
+
+    const a = document.createElement('a');                          /*Skapar ett ankare*/
+    a.href = url;                                                   /*Ger ankaret en referens*/
+    a.download = 'shoppingList.txt';                                /*Ger ankaren ett download-atttribut*/
+    document.body.appendChild(a);                                   /*Fäster ankaret i DOM*/
+    a.click();                                                      /*Klickar ankaret (utan klick) = laddar ner*/
+    document.body.removeChild(a);                                   /*Tar bort anakert efter nedladdning*/
+    URL.revokeObjectURL(url);                                       /*Tar bort url med lokal data*/
+});
